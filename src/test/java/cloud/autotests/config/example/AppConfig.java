3888362bc7c96e2083e939f87829d09b126cdaaa
olexandrso <alex.worksot@gmail.com>
2022-08-25 11:49:24 +0300

    new structure

---
+++ src/test/java/cloud/autotests/config/example/AppConfig.java
@@ -0,0 +1,17 @@
+package cloud.autotests.config.example;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/app.properties"
+})
+public interface AppConfig extends Config {
+
+    String webUrl();
+    String apiUrl();
+    String userLogin();
+    String userPassword();
+
+}
